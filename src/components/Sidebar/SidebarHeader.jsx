e9362d4dc7d7784154379ab578406fa9ceba3e5e
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2023-03-24 10:52:05 +0000

    Add name, icon and logo

--- src/components/Sidebar/SidebarHeader.jsx
+++ src/components/Sidebar/SidebarHeader.jsx
@@ -1,7 +1,20 @@
+import {FaChartPie} from "react-icons/fa";
+import {FiBell, FiSearch, FiArrowLeft} from "react-icons/fi";
 
 const SidebarHeader = () => {
     return (
-        <div>Header</div>
+        <div className="header d-flex align-items-center space-between p-10">
+            <div className="d-flex justify-content-center align-items-center g-10">
+                <FaChartPie className="icon-pie-chart" />
+                <h1>Decade</h1>
+            </div>
+            
+            <div className="d-flex justify-content-center align-items-center g-5">
+                <FiBell className="header-icons" />
+                <FiSearch className="header-icons"/>
+                <FiArrowLeft className="header-icons"/>
+            </div>
+        </div>
     )
 };
 
