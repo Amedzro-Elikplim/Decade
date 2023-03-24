7c5c39a9158bbfd019aeb47dcfbb208456c8e190
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2023-03-24 09:02:38 +0000

    Add dashboard

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,17 +3,13 @@ import './App.css'
 import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 
 const Routes = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />
-  },
-  {
-    path: "/login",
-    element: <Login />
-  }
+  { path: "/", element: <Home /> },
+  { path: "/login", element: <Login /> },
+  { path:"/dashboard", element: <Dashboard /> }
 ]);
 
 export default Routes;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,19 @@
+import "./login.css";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+
+    const navigate = useNavigate();
+
+    const handleNavigate = () => {
+        navigate("/dashboard");
+    }
+
     return (
-          <div className="d-flex justify-content-center align-items-center">
-            home
+          <div className="d-flex justify-content-center align-items-center h-100">
+            <div className="login-card d-flex align-items-center justify-content-center">
+                <button className="p-10" onClick={() => handleNavigate()}>Login</button>
+            </div>
           </div>
     )
 };
